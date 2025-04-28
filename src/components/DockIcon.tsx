e3afbc7b7fdf560db76bc2ad66761c2ad70ff5fd
Dwaine Matthew <dwainejade@gmail.com>
2025-04-28 01:05:03 -0400

    remove animation

--- src/components/DockIcon.tsx
+++ src/components/DockIcon.tsx
@@ -20,15 +20,11 @@ const DockIcon: React.FC<DockIconProps> = ({
   onClick,
   isOpen,
 }) => {
-  // Add hover animation
-  const hoverScale = 1.2;
-
   return (
     // motion.div enables Framer Motion animations
     <motion.div
       className={styles.dockIcon}
-      whileHover={{ scale: hoverScale, y: -10 }} // Scale up and move up slightly on hover
-      whileTap={{ scale: 0.9 }} // Scale down slightly when clicked
+      whileTap={{ filter: "brightness(.6)" }} // Scale down slightly when clicked
       onClick={onClick}
       title={label} // Tooltip on hover
       data-window-id={id} // Add data attribute for targeting in minimize animation
